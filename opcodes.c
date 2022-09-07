bba7ed5b01b65f41506b3131fb1e5f4e1ed8d8db
Kamal Hussein <kamalinuxx@gmail.com>
2022-09-07 12:03:50 -0700

    Implement the pint opcode

--- opcodes.c
+++ opcodes.c
@@ -58,3 +58,25 @@ void _pall(stack_t **doubly, unsigned int cline)
 		aux = aux->next;
 	}
 }
+
+/**
+ * _pint - prints the value at the top of the stack
+ *
+ * @doubly: head of the linked list
+ * @cline: line number
+ * Return: no return
+ */
+void _pint(stack_t **doubly, unsigned int cline)
+{
+	(void)cline;
+
+	if (*doubly == NULL)
+	{
+		dprintf(2, "L%u: ", cline);
+		dprintf(2, "can't pint, stack empty\n");
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", (*doubly)->n);
+}
