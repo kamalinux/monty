2d9b01a830b509afc39549d24c1ed06cb596330d
Kamal Hussein <kamalinuxx@gmail.com>
2022-09-07 07:29:39 -0700

    Fixing betty style

--- get_opcodes.c
+++ get_opcodes.c
@@ -1,5 +1,12 @@
 #include "monty.h"
 
+/**
+ * get_opcodes - selects the correct opcode to perform
+ *
+ * @opc: opcode passed
+ *
+ * Return: pointer to the function that executes the opcode
+ */
 void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number)
 {
 	instruction_t instruct[] = {
@@ -8,6 +15,7 @@ void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number)
 		{NULL, NULL}
 	};
 	int i;
+
 	for (i = 0; instruct[i].opcode; i++)
 	{
 		if (_strcmp(instruct[i].opcode, opc) == 0)
