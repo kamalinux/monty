13e8c611431eaa5f87baa197e7749371cd97c2fc
Kamal Hussein <kamalinuxx@gmail.com>
2022-09-08 13:55:59 -0700

    Implement the pstr opcode

--- get_opcodes.c
+++ get_opcodes.c
@@ -22,6 +22,7 @@ void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number)
 		{"mul", _mul},
 		{"mod", _mod},
 		{"pchar", _pchar},
+		{"pstr", _pstr},
 		{NULL, NULL}
 	};
 	int i;
